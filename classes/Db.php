e58beffb4ef2dd805e53c79bb439a95a91931185
Lieselotte-K <lieselottekeuleers@hotmail.com>
2020-03-27 22:18:49 +0100

    update2

--- classes/Db.php
+++ classes/Db.php
@@ -1,17 +1,16 @@
 <?php
+    class Db{
 
-class Db{
-    private static $conn;
+        private static $conn;
 
-    public static function getConnection(){
-        include_once(__DIR__ . "/../settings/settings.php");
-
-        if(self::$conn === null){
-            self::$conn = new PDO('mysql:host=' . SETTINGS['db']['host'] . ';dbname=' . SETTINGS['db']['database'], SETTINGS['db']['user'], SETTINGS['db']['password']);
-            return self::$conn;
-        }
-        else{
-            return self::$conn;
+        public static function getConnection(){
+            if(self::$conn===null){
+                self::$conn = newPDO ('mysql...');
+                return self::$conn;
+            } else{
+                return self::$conn;
+            }
         }
     }
-}
+
+?>
