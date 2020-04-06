39359955055b2fadf5f7db6ce04fd78341ce2a90
Jadabyte <Jadabyte>
2020-04-06 16:52:16 +0200

    removed session() from home.php and removed unnecessary commented code in nav.inc.php

--- nav.inc.php
+++ nav.inc.php
@@ -1,25 +1,6 @@
-<?php
-    include_once(__DIR__ . "/classes/Search.php");
-
-    /*if(!empty($_POST)){
-        try {
-            $search = new Search();
-            $search->setSearchItem($_POST['search']);
-        } catch (\Throwable $th) {
-            $error = $th->getMessage();
-        }
-    }*/
-
-
-?><nav class="navbar">
+<nav class="navbar">
     <a href="index.php">Home</a>
     <a href="mylist.php">Profile</a>
     <a href="seeFriends.php">View Buddies</a>
-    
-    <!--<form action="" method="post">
-      <input type="text" placeholder="Search for people or interests" name="search">
-      <input type="submit" value="Search">
-    </form>-->
-    
     <a href="logout.php" class="navbar__logout">Logout</a>
 </nav>
