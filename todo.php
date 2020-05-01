1ee48b68a0dd8b050e3c6d09d801d531a4bb9893
Jadabyte <Jadabyte>
2020-05-01 18:30:53 +0200

    todo.php and todo.js added

---
+++ todo.php
@@ -0,0 +1,21 @@
+<?php
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo</title>
+</head>
+<body>
+    <ul class="todosList">
+        <li>This is a list item</li>
+    </ul>
+
+    <div>
+        <input type="text" id="todoContent" placeholder="Create a new Todo">
+        <a href="#" id="btnAddTodo">Add</a>
+    </div>
+    <script src="js/todo.js"></script>
+</body>
+</html>
